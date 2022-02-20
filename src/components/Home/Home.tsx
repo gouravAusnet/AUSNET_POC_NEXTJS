fd474c866128f4bc0aca4216f09e56b8738cb1d3
GMX\002NHH744 <Gourav.Bahare@ibm.com>
2022-02-20 15:13:20 +0530

    banner componenet modification

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import { StyleguideComponentProps } from 'lib/component-props';
 import banner1 from '../../../public/AusnetAssets/banner1.png';
 import banner2 from '../../../public/AusnetAssets/banner2.png';
 import banner3 from '../../../public/AusnetAssets/banner3.png';
+import arrowButton from '../../../public/AusnetAssets/arrowButton.png';
 import Image from 'next/image';
 
 type HomeProps = StyleguideComponentProps & {
@@ -26,8 +27,8 @@ const Home = (props: HomeProps): JSX.Element => (
       }
     </div>
     <div className='right-section'>
-      <p style={{display: 'grid'}}>
-        <span className='header-title common-title'>Connecting</span> 
+      <p style={{ display: 'grid' }}>
+        <span className='header-title common-title'>Connecting</span>
         <span className='header-title change-title'>Communities</span>
       </p>
       <RichText className='para-text' field={props.fields.contentCommunities} />
@@ -36,16 +37,37 @@ const Home = (props: HomeProps): JSX.Element => (
           <Text field={props.fields.improveNetwork} />
         </p>
         <RichText className='paraImprove-text improveNetwork-text' field={props.fields.improveNetworkText} />
-        <div style={{display: 'flex', marginLeft: '60px'}}>
-          <span className='outages-section'>12<br/><span className='outages-innersection'>Planned outages</span></span>
-          <span className='outages-section'>4<br/><span className='outages-innersection'>Unplanned outages</span></span>
-          <span className='outages-section last-outages'>16<br/><span className='outages-innersection'>Restored today</span></span>
+        <div style={{ display: 'flex', marginLeft: '60px' }}>
+          <span className='outages-section'>12<br /><span className='outages-innersection'>Planned outages</span></span>
+          <span className='outages-section'>4<br /><span className='outages-innersection'>Unplanned outages</span></span>
+          <span className='outages-section last-outages'>16<br /><span className='outages-innersection'>Restored today</span></span>
         </div>
       </div>
       <p className='sub-text common-title'>
         <Text field={props.fields.heading} />
       </p>
       <RichText className='para-text' field={props.fields.commonCommunities} />
+      <ul className='ulinks'>
+        <li className='links'>
+          <div className="arrowStyle">
+            <Image className="d-block w-100" src={arrowButton} alt="AusNet slide" />
+          </div>
+
+          Connect my electricity
+        </li>
+        <li className='links'>
+          <div className="arrowStyle">
+            <Image className=" arrowStyle d-block w-100" src={arrowButton} alt="AusNet slide" />
+          </div>
+          Connect my solar
+        </li>
+        <li className='links'>
+          <div className="arrowStyle">
+            <Image className="arrowStyle d-block w-100" src={arrowButton} alt="AusNet slide" />
+          </div>
+          Connect my gas
+        </li>
+      </ul>
     </div>
   </div>
 );
